835f8010069a00fd397cd1d351b9b7c9bd0963ba
william_ws7 <alexander171296@gmail.com>
2020-02-12 00:47:14 -0400

    agg categories

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Header from '../components/Header';
-import Search from '../components/Search'
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 import '../assets/styles/app.scss';
 import '../assets/styles/components/Search.scss';
 import '../assets/styles/components/Header.scss';
 const App = () => (
 
-     <div className="App">
-         <Header />
-         <Search />
-     </div>
+    <div className="App">
+        <Header />
+        <Search />
+        <Categories>
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+    </div>
 );
 
 export default App;
